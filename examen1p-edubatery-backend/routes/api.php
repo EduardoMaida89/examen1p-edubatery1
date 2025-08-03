7deb4867cb001ceb85827a381d008f305974d792
EduardoMaida89 <aeduardo.maid@gmail.com>
2025-08-03 19:08:52 -0400

    Implementacion de rutas login y register

--- examen1p-edubatery-backend/routes/api.php
+++ examen1p-edubatery-backend/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
